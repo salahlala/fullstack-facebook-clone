e158209c142b710acfc9679b3891aea665ffdc6c
salahlala <salahlala303@gmail.com>
2024-09-14 04:28:23 +0300

    refactor(stor): clean the code

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -4,14 +4,12 @@ import authSlice from "@store/authSlice";
 import uiSlice from "@store/uiSlice";
 import dialogUiSlice from "@store/dialogUiSlice";
 import socketSlice from "./socketSlice";
-import messengerSlice from "@store/messengerSlice";
 const rootReducer = combineReducers({
   [apiSlice.reducerPath]: apiSlice.reducer,
   auth: authSlice,
   ui: uiSlice,
   dialog: dialogUiSlice,
   socket: socketSlice,
-  messenger: messengerSlice,
 });
 
 export const store = configureStore({
@@ -19,19 +17,10 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Disable the check
-      // serializableCheck: {
-      //   ignoredPaths: ["socket.socket"],
-      //   ignoredActions: ["socket/setSocket"],
-      // },
     }).concat(apiSlice.middleware),
   devTools: true,
 });
 
-// store.subscribe(() => {
-//   const state = store.getState();
-//   if (!state.auth.isLoggedIn) {
-//     store.dispatch(apiSlice.util.resetApiState());
-//   }
-// });
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
